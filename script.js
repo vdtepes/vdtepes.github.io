39ac8c2f22987a51457c3bd7f1ab3a5e50fb1756
vdtepes <114120682+vdtepes@users.noreply.github.com>
2023-02-20 21:37:28 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('nav-links')[0]
-
-toggleButton.addEventListener('click', () =>
-{
-  navbarLinks.classList.toggle('active')
-}
-)
-console.log(window.getComputedStyle(document.documentElement).getPropertyValue('--text-color'))
